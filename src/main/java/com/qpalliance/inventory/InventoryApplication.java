16f14801d33365bc56663805851682bb39d46e3c
LabsCode <labscode2026@outlook.com>
2025-04-29 11:50:31 -0500

    clase principal creada

--- src/main/java/com/qpalliance/inventory/InventoryApplication.java
+++ src/main/java/com/qpalliance/inventory/InventoryApplication.java
@@ -1,4 +1,12 @@
 package com.qpalliance.inventory;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class InventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+    }
 }
